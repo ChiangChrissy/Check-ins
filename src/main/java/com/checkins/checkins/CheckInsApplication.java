b5970f43b2684a6bc77b5f51ac26a44a843b2cc1
aliencindy <lucy8611892@gmail.com>
2023-03-22 14:34:51 +0800

    Initial commit

---
+++ src/main/java/com/checkins/checkins/CheckInsApplication.java
@@ -0,0 +1,13 @@
+package com.checkins.checkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInsApplication.class, args);
+	}
+
+}
